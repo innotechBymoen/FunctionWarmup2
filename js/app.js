2d937059bc587fe5dd357a151ffc4db730f962b2
bymoen <alexb@innotechcollege.com>
2023-03-23 17:45:24 -0600

    Learning some more Js selectors

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 let image_tag = document.querySelector(`img`);
 let last_content = document.querySelector(`#last_content`);
 let content_tags = document.querySelectorAll(`.text_content`);
+
+let know_container = document.getElementById(`know_container`);
+let know_content = document.getElementsByClassName(`know_content`);
